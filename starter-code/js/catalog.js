4742214d91041ccca12497ad851b58eb0d76ad0b
richjimdev <richjimdev@gmail.com>
2018-08-16 14:23:13 -0700

    more downdown functionality

--- starter-code/js/catalog.js
+++ starter-code/js/catalog.js
@@ -8,7 +8,6 @@ var cart = new Cart([]);
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
-
   // select id="items"
   //TODO: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
@@ -18,16 +17,14 @@ function populateForm() {
     itemOnList.value = product.name;
     selectElement.appendChild(itemOnList);
   }
-
 }
 
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
-
   // TODO: Prevent the page from reloading
-
+  event.preventDefault();
   // Do all the things ...
   addSelectedItemToCart();
   cart.saveToLocalStorage();
